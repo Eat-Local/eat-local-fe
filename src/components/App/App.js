34619565c13828bf22aad877e5889febb2f5802d
Anthony Shellman <atshellman@gmail.com>
2022-12-05 15:44:45 -0700

    Comment unused imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
-import { useState, useEffect } from "react"
-import FavoritesPage from "../FavoritesPage/FavoritesPage";
-import Footer from "../Footer/Footer";
-import LandingPage from "../LandingPage/LandingPage";
-import Nav from "../Nav/Nav";
-import ResultCard from "../ResultCard/ResultCard";
-import ResultsPage from "../ResultsPage/ResultsPage";
-import SingleResultPage from "../SingleResultPage/SingleResultPage";
-
+// import { useState, useEfffect } from "react"
+// import FavoritesPage from "../FavoritesPage/FavoritesPage";
+// import Footer from "../Footer/Footer";
+// import LandingPage from "../LandingPage/LandingPage";
+// import Nav from "../Nav/Nav";
+// import ResultCard from "../ResultCard/ResultCard";
+// import ResultsPage from "../ResultsPage/ResultsPage";
+// import SingleResultPage from "../SingleResultPage/SingleResultPage";
 import './App.css';
 
 const App = () => {
-  const [ restaurants, setRestaurants ] = useState([])
+  // const [ restaurants, setRestaurants ] = useState([])
 
   return (
     <main>
