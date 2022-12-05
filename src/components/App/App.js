d2e2b73266509a42aeb7cd3ff921bd012b87d2a4
Anthony Shellman <atshellman@gmail.com>
2022-12-05 15:25:08 -0700

    Add import statements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,13 @@
 import { useState, useEffect } from "react"
+import FavoritesPage from "../FavoritesPage/FavoritesPage";
+import Footer from "../Footer/Footer";
+import LandingPage from "../LandingPage/LandingPage";
+import Nav from "../Nav/Nav";
+import ResultCard from "../ResultCard/ResultCard";
+import ResultsPage from "../ResultsPage/ResultsPage";
+import Search from "../Search/Search";
+import SingleResultPage from "../SingleResultPage/SingleResultPage";
+
 import './App.css';
 
 const App = () => {
