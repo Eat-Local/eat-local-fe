569835cfa348d24a08785216066d2791e1335ddf
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-12-06 13:57:58 -0700

    change onClick for form to arrow function

--- src/components/FavoritesPage/FavoritesPage.js
+++ src/components/FavoritesPage/FavoritesPage.js
@@ -7,6 +7,8 @@ import React from 'react'
 const FavoritesPage = () => {
   const [searchFavorites, setSearchFavorites] = useState('')
 
+
+
   const handleClick = (event) => {
     event.preventDefault()
   }
@@ -20,7 +22,7 @@ const FavoritesPage = () => {
           value={searchFavorites} 
           onChange={(event) => setSearchFavorites(event.target.value)} 
           required/>
-        <button disabled={searchFavorites.length<1} type='submit' className='submit' onClick={handleClick}>GO</button>
+        <button disabled={searchFavorites.length<1} type='submit' className='submit' onClick={(event) => handleClick(event)}>GO</button>
         </form>
       
     </div>
