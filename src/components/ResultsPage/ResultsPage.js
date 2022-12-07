bb6daff26d8d44791e924a2b23c4bc3952e5b1d5
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-12-06 18:02:06 -0700

    Add iteration of business to create business cards

--- src/components/ResultsPage/ResultsPage.js
+++ src/components/ResultsPage/ResultsPage.js
@@ -1,8 +1,25 @@
 import "./ResultsPage.css"
+import { data } from "../../assets/mock-data"
+
+import React from 'react'
+import ResultCard from "../ResultCard/ResultCard"
 
 const ResultsPage = () => {
-  
-
+  const businessCard = data.map((business, index) => {
+    return (
+      <ResultCard
+        title={business.attributes.title}
+        rating={business.attributes.rating}
+        photo={business.attributes.img}
+        key={index}
+      />
+    )
+  })
+  return (
+    <div className="results-container">
+      {businessCard}
+    </div>
+  )
 }
 
 export default ResultsPage
