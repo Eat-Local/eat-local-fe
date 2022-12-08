21c353bf27b25457a78ab1a3749932b35c77f4c1
Anthony Shellman <atshellman@gmail.com>
2022-12-07 20:05:09 -0700

    Add router functionality, data rendering

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -1,12 +1,15 @@
 import "./ResultCard.css"
+import { Link } from "react-router-dom"
 
-const ResultCard = ({title, photo, rating}) => {
+const ResultCard = ({title, photo, rating, id}) => {
   return (
+    <Link to={`/results/${id}`}>
     <article className="business-card">
       <img className="business-card-image" src={photo} alt={title}></img>
       <p>{title}</p>
       <p>{rating}</p>
     </article>
+    </Link>
   )
 }
 
--- src/components/SingleResultPage/SingleResultPage.js
+++ src/components/SingleResultPage/SingleResultPage.js
@@ -1,12 +1,12 @@
 import "./SingleResultPage.css";
-import { data } from "../../assets/mock-data";
+// import { data } from "../../assets/mock-data";
 
 const placeholder = require('../../assets/eatlocalicon.png')
 
-const SingleResultPage = () => {
-  const { address, description, hours, img, phone, rating, site, title, wheelchair_accessible } = data[0].attributes;
+const SingleResultPage = ({ business }) => {
+  const { hours, img, phone, rating, site, title } = business.attributes;
   const altText = `A photo describing ${title}'s business, provided by ${title}`
-  const accessibility = wheelchair_accessible ? "Yes" : "No";
+  // const accessibility = wheelchair_accessible ? "Yes" : "No";
 
   return(
     <section className="single-result-section">
@@ -14,12 +14,12 @@ const SingleResultPage = () => {
         <img className="business-photo" src={img} alt={altText} />
         <div className="information-container">
           <h1>{title}</h1>
-          <p>{description}</p>
+          {/* <p>{description}</p> */}
           <p>{rating}</p>
-          <p>{address}</p>
+          {/* <p>{address}</p> */}
           <p>{hours}</p>
           <p>{phone}</p>
-          <p>Wheelchair Accessible: {accessibility}</p>
+          {/* <p>Wheelchair Accessible: {accessibility}</p> */}
           <p>Check out {title}'s <a href={site} target="_blank" rel="noopener noreferrer">website</a>!</p>
         </div>
         <img className="favorite-icon" src={placeholder} alt="an outline of a star" />
