37b5247f25277b170561dedd45143508ec192a0a
Victoria Fields <victoriaashleyfields@gmail.com>
2022-12-08 10:37:44 -0700

    Add UI login dropdown functionality

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react';
 import Search from "../Search/Search";
 import { Link } from "react-router-dom"
 import "./Nav.css";
@@ -5,23 +6,50 @@ import "./Nav.css";
 const logo = require('../../assets/eatlocalicon.png');
 
 const Nav = ( { business, setBusiness, location, setLocation, onSearch } ) => {
+  const [openLogin, setOpenLogin] = useState(false);
+  const ref = useRef(null);
+
+  const handleClick = (event) => {
+    setOpenLogin(!openLogin);
+  }
+
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      setOpenLogin(false);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    }
+  }, [ref, setOpenLogin])
+
   return(
-    <nav className="main-nav">
-      <Link to="/">
-        <img src={logo} alt="Eat Local logo, a green location marker with a bite taken out of it!" />
-      </Link>
-      <Search
-        business={business}
-        setBusiness={setBusiness}
-        location={location}
-        setLocation={setLocation}
-        onSearch={onSearch}
-      />
-      <div className="greeting-menu-container">
-        <span className="greeting">Hello, username!</span>
-        <span className="menu">Login | Favorites</span>
-      </div>
-    </nav>
+    <>
+      <nav className="main-nav">
+        <Link to="/">
+          <img src={logo} alt="Eat Local logo, a green location marker with a bite taken out of it!" />
+        </Link>
+        <Search
+          business={business}
+          setBusiness={setBusiness}
+          location={location}
+          setLocation={setLocation}
+          onSearch={onSearch}
+        />
+        <div className="greeting-menu-container">
+          <span className="greeting">Hello, username!</span>
+          <span className="menu"><span onClick={(event) => handleClick(event)}>Login</span> | Favorites</span>
+        </div>
+      </nav>
+      {openLogin && <div className="login-container" ref={ref}>I am Login</div>}
+    </>
   )
 }
 
