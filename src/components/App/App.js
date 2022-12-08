1027cf4e7c5258c021d320a365790e8b7396ef4f
Victoria Fields <victoriaashleyfields@gmail.com>
2022-12-07 17:31:12 -0700

    Add potential logic for featured busineses, correct spell err in footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Nav from "../Nav/Nav";
 import LandingPage from "../LandingPage/LandingPage";
 import Footer from "../Footer/Footer";
@@ -12,17 +12,29 @@ const App = () => {
   // const [ restaurants, setRestaurants ] = useState([]);
   const [ location, setLocation ] = useState('');
   const [ business, setBusiness ] = useState('restaurant');
+  const [ featured, setFeatured ] = useState([]);
   const [ results, setResults ] = useState([]);
+
+  const genRandomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  useEffect(() => {
+    fetch(`https://throbbing-wood-3534.fly.dev/api/v1/business?business=$restaurant&location=denver`)
+      .then(data => data.json())
+      .then(restaurants => {
+        console.log(restaurants);
+        // use random num gen and length of restaurants to find a random denv rest
+        // set a first element in featured array
+        // repeat this logic for brewery and grocery?
+        // could add random logic for other locations if we felt inclined
+      })
+  }, [])
 
   const onSearch = (business, searchQuery) => {
     console.log(`https://throbbing-wood-3534.fly.dev/api/v1/business?business=${business}&location=${searchQuery}`);
-    fetch(`https://throbbing-wood-3534.fly.dev/api/v1/business?business=${business}&location=${searchQuery}`,
-      {
-        method: 'GET',
-        headers: {
-          "Access-Control-Allow-Origin": "*"
-        },
-     })
+    
+    fetch(`https://throbbing-wood-3534.fly.dev/api/v1/business?business=${business}&location=${searchQuery}`)
       .then(data => data.json())
       .then(data => console.log(data))
   }
@@ -37,7 +49,6 @@ const App = () => {
         onSearch={onSearch}
       />
       <LandingPage />
-      {/* <SingleResultPage /> */}
       <Footer/>
     </main>
   )
