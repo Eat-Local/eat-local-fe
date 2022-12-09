a5893bf5773b214408d22979396f154484ee9a2a
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-12-09 10:56:29 -0700

    change styling of marker and format address

--- src/components/SingleResultPage/SingleResultPage.js
+++ src/components/SingleResultPage/SingleResultPage.js
@@ -1,25 +1,23 @@
 import "./SingleResultPage.css";
 import GoogleMapReact from 'google-map-react';
-import {ImLocation} from 'react-icons/im'
+import {MdLocationPin} from 'react-icons/md'
 import {AiOutlineStar} from 'react-icons/ai'
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Marker = ({ text }) => <div>{text}</div>
 
 const SingleResultPage = ({ business }) => {
   const { img, display_phone, rating, site, title, price, coordinates, display_address } = business.attributes;
   const phone = display_phone.replace(/[^\d]/g, '');
-  console.log(display_phone)
-  console.log(business)
+  const address = display_address.display_address.map((element) => `${element} `)
   const altText = `A photo describing ${title}'s business, provided by ${title}`
   const defaultProps = {
     center: {
       lat: coordinates.latitude,
       lng: coordinates.longitude
     },
-    zoom: 11
+    zoom: 12
   }
-  console.log(apiKey)
 
   return(
     <section className="single-result-section">
@@ -29,7 +27,7 @@ const SingleResultPage = ({ business }) => {
           <h1>{title}</h1>
           {price}
           <p>Rating: {rating}/5</p>
-          <p>{display_address.display_address}</p>
+          <p>{address}</p>
           <a href={phone}>{display_phone}</a>
           <p>Check out {title}'s <a href={site} target="_blank" rel="noopener noreferrer">website</a>!</p>
         </div>
@@ -41,10 +39,14 @@ const SingleResultPage = ({ business }) => {
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent
+        <Marker
           lat={coordinates.latitude}
           lng={coordinates.longitude}
-          text={<ImLocation/>}
+          text={<MdLocationPin style={{
+            fontSize: "30px",
+             color: "#408021"
+          }}
+          />}
         />
       </GoogleMapReact>
     </div>
