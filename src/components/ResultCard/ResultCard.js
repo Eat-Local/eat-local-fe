307fb77f2a13b8341b6e1c2d8340e21c95f2f9b9
Victoria Fields <victoriaashleyfields@gmail.com>
2022-12-09 23:07:11 -0700

    Add functional delete fav

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -2,28 +2,31 @@ import { Link } from "react-router-dom";
 import { AiOutlineStar } from 'react-icons/ai';
 import "./ResultCard.css";
 
-const ResultCard = ({title, photo, rating, id, alias, user, addFavorite, deleteFavorite, business}) => {
-  const inUserFavs = user.favorites.reduce((acc, favorite) => {
-    console.log('favorite.title in reduce: ', favorite.title)
-    if (favorite.title === title) {
-      acc = true;
-    }
-    return acc;
-  }, false)
+const ResultCard = ({title, photo, rating, id, alias, user, addFavorite, deleteFavorite, business}) => { 
+  let inUserFavs = false;
   
-  const buttonTxt = inUserFavs ? 'delete from favorites' : 'add to favorites'
-
-  const handleClick = (event) => {
-    const userId = user.id;
-    addFavorite(business, userId);
+  if (user) {
+    inUserFavs = user.favorites.reduce((acc, favorite) => {
+      if (favorite.title === title) {
+        acc = true;
+      }
+      return acc;
+    }, false)
   }
+  
+  const handleDelete = () => {
+    const currentFavorite = user.favorites.find(favorite => favorite.title === title)
+    deleteFavorite(parseInt(currentFavorite.id));
+  }
+
+  const buttonTxt = inUserFavs ? 'delete from favorites' : 'add to favorites'
 
   return (
     <article className="business-card">
       <Link to={`/results/${alias}`}><img className="business-card-image" src={photo} alt={title}></img></Link>
       <p>{title}</p>
       <p>{rating}</p>
-      {user && <button onClick={(event) => handleClick(event)}>{buttonTxt}</button>}
+      {user && <button onClick={inUserFavs ? handleDelete : () => addFavorite(business, user.id)}>{buttonTxt}</button>}
     </article>
   )
 }
