a181bef609526edbef6da663043d5d3fcf52abb7
Anthony Shellman <atshellman@gmail.com>
2022-12-11 21:51:53 -0700

    Add data-cy property

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -28,10 +28,10 @@ const LandingPage = ({ user, addFavorite, deleteFavorite, featured, featError, f
   return(
     <section className="landing-section">
       <div className="overview-container">
-        <span>
+        <span data-cy="overview">
         Eat local was designed to exclusively support local businesses.  Unlike larger companies, small businesses do not have 
         the resources or the capital to survive without sales and support.  Approximately 1 out of 5 small businesses close their 
-        doors permanently within their first year of business.  The odds of a small business failing increase more each year they are open–with a 
+        doors permanently within their first year of business.  The odds of a small business failing increase more each year they are open-with a 
         failure rate of 66% after 10 years of business.  Although running a small business is very difficult, it is near impossible without 
         support. To continue to have diverse options to eat, drink, and shop, there needs to be an effort made to support these businesses or 
         your favorite neighborhood spots may not be around much longer.  Please eat local!
