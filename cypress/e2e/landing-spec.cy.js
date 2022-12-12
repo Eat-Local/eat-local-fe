f946c88f9bde441cc91cb6349b1385821faaad71
Anthony Shellman <atshellman@gmail.com>
2022-12-11 21:31:34 -0700

    Rework testing for nav bar

--- cypress/e2e/landing-spec.cy.js
+++ cypress/e2e/landing-spec.cy.js
@@ -9,11 +9,9 @@ describe('Landing Page', () => {
     cy.intercept('GET', Cypress.env('brewery'), {
       fixture: 'breweryData.json'
     })
+    cy.visit('/')
   })
 
-it('passes', () => {
-  cy.visit('https://example.cypress.io')
-})
 /* Test banner contents 
   - Test for logo
   - Test for search bar
@@ -23,9 +21,35 @@ it('passes', () => {
   - Test for "Home | Login | Favorites"
 */ 
 
-// Test Typing in search bar
+it('has a navbar with various features and functionality', () => {
+  cy.get('[data-cy="navbar"]')
+  .within(() => {
+    cy.get('[data-cy="logo"]').should('be.visible')
+    cy.get('[data-cy="search"]').should('be.visible')
+    .type('hello').should('have.value', 'hello')
+    cy.get('[data-cy="market-radio"]').check()
+    cy.get('[data-cy="brewery-radio"]').check()
+    cy.get('[data-cy="restaurant-radio"]').check()
+    cy.get('[data-cy="submit"]').should('be.visible')
+    cy.get('[data-cy="greeting"]').contains('Hello, Friend!')
+    cy.get('[data-cy="menu"]').contains('Home')
+    cy.get('[data-cy="menu"]').contains('Login')
+    cy.get('[data-cy="menu"]').contains('Favorites')
+  })
+})
 
-// Test radio buttons
+it('has an overview blurb', () => {
+  cy.get('[data-cy="overview"]')
+  .contains("Eat local was designed to exclusively support local businesses.  Unlike larger companies, small businesses do not have the resources or the capital to survive without sales and support.  Approximately 1 out of 5 small businesses close their doors permanently within their first year of business.  The odds of a small business failing increase more each year they are open-with a failure rate of 66% after 10 years of business.  Although running a small business is very difficult, it is near impossible without support. To continue to have diverse options to eat, drink, and shop, there needs to be an effort made to support these businesses or your favorite neighborhood spots may not be around much longer.  Please eat local!")
+})
+
+it('has featured local businesses', () => {
+  
+})
+
+it('has a footer with team info', () => {
+  
+})
 
 // Test for overview
 
