6b628dd8f72ef8e0c0253f2346ecb513635636e1
Victoria Fields <victoriaashleyfields@gmail.com>
2022-12-12 10:46:54 -0700

    Remove unused state

--- src/components/FavoritesPage/FavoritesPage.js
+++ src/components/FavoritesPage/FavoritesPage.js
@@ -11,46 +11,6 @@ const FavoritesPage = ({ user, deleteFavorite }) => {
     displaySearch = true;
   }
 
-  // if (!user) {
-  //   favorites = "You should log in"
-  // } else if (user.favorites.length === 0) {
-  //   favorites = "You dont have any favorites yet"
-  // } else if (!searchFavorites) {
-  //   favorites = user.favorites.map((favorite) => {
-  //     const { id, title, rating, image, alias} = favorite;
-  //     return (
-  //       <ResultCard
-  //         key={id}
-  //         id={id}
-  //         title={title}
-  //         rating={rating}
-  //         photo={image}
-  //         alias={alias}
-  //         user={user}
-  //         deleteFavorite={deleteFavorite}
-  //         displayType="favorite"
-  //       />
-  //     )
-  //   })
-  // } else {
-  //   const filteredFavorites = user.favorites.filter(favorite => favorite.title.toLowerCase().includes(searchFavorites.toLocaleLowerCase()));
-  //   favorites = filteredFavorites.map((favorite) => {
-  //     const { id, title, rating, image, alias} = favorite;
-  //     return (
-  //       <ResultCard
-  //         key={id}
-  //         id={id}
-  //         title={title}
-  //         rating={rating}
-  //         photo={image}
-  //         alias={alias}
-  //         user={user}
-  //         displayType="favorite"
-  //       />
-  //     )
-  //   })
-  // }
-
   const displayFavorites = (curUser, searchFavs) => {
     let display;
     if (!curUser) {
